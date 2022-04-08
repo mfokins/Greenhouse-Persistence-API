9a9dea6e4d2e5d9ad56f6bd699eef4c32ec69c2d
KlausBenislavskis <klavs.benislavskis@inbox.lv>
2022-04-08 16:20:27 +0200

    Started the core

--- Core/Interfaces/Temperature/ITemperatureRepository.cs
+++ Core/Interfaces/Temperature/ITemperatureRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Core.Interfaces.Temperature
 {
-    internal interface ITemperatureRepository
+    public interface ITemperatureRepository
     {
+        void Add(TemperatureMesurment temperature);
     }
 }
