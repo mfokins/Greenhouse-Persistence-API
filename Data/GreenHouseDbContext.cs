181d3a53a1de99c9343185f23b746f605861eccd
nickcernean <nicolaecer@gmail.com>
2022-05-21 12:01:26 +0200

    checkout commit

--- Data/GreenHouseDbContext.cs
+++ Data/GreenHouseDbContext.cs
@@ -18,7 +18,6 @@ namespace Data
             optionsBuilder.UseSqlServer("Server=database-2.citlownhihib.eu-central-1.rds.amazonaws.com,1433;Database=GreenHouse;User Id=admin; Password=12345678;");
         }
 
-
-
+       
     }
 }
