37e82b847e6a6842f7b25ed55d553c435a874fc0
nickcernean <nicolaecer@gmail.com>
2022-05-30 21:44:33 +0200

    coordinates migration

---
+++ Data/Migrations/20220530150119_CoordinatesMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class CoordinatesMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Latitude",
+                table: "Greenhouses",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Longitude",
+                table: "Greenhouses",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Greenhouses");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Greenhouses");
+        }
+    }
+}
