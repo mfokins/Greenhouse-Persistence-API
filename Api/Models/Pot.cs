8921ac6bc669f6b61d921d2023072df0f0fd7af0
Klavs Benislavskis <klavs.benislavskis@inbox.lv>
2022-05-30 10:41:08 +0200

    Quick fix part 3

--- Api/Models/Pot.cs
+++ Api/Models/Pot.cs
@@ -4,7 +4,7 @@ namespace Api.Models
 {
     public class Pot
     {
-        public double? LatestMoisture { get; set; }
+        public double LatestMoisture { get; set; }
         public string Name { get; set; }
         public double LowerMoistureThreshold { get; set; }
         public int Id { get; set; }
