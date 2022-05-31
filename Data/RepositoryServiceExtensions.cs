c1e33d762da166fff3394321e16a4e57142b7721
KlausBenislavskis <klavs.benislavskis@inbox.lv>
2022-05-31 13:49:36 +0200

    Refactored Repositry dependecy injection

---
+++ Data/RepositoryServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿
+using Core.Interfaces;
+using Core.Interfaces.DioxideCarbon;
+using Core.Interfaces.Greenhouse;
+using Core.Interfaces.Humidity;
+using Core.Interfaces.Pot;
+using Core.Interfaces.Sensors;
+using Core.Interfaces.Temperature;
+using Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Data
+{
+    public static class RepositoryServiceExtensions
+    {
+        public static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITemperatureRepository, TemperatureRepository>();
+            services.AddScoped<IThresholdRepository, ThresholdRepository>();
+            services.AddScoped<IHumidityRepository, HumidityRepository>();
+            services.AddScoped<IDioxideCarbonRepository, DioxideCarbonRepository>();
+            services.AddScoped<IMoistureRepository, MoistureRepository>();
+            services.AddScoped<IPotRepository, PotRepository>();
+            services.AddScoped<ISensorRepository, SensorRepository>();
+            services.AddScoped<IGreenhouseRepository, GreenhouseRepository>();
+
+            return services;
+        }
+    }
+}
--- DataServer/Program.cs
+++ DataServer/Program.cs
@@ -1,44 +1,20 @@
 using Api.BridgeIot;
 using Core;
-using Core.Interfaces;
-using Core.Interfaces.DioxideCarbon;
-using Core.Interfaces.Greenhouse;
-using Core.Interfaces.Humidity;
-using Core.Interfaces.Pot;
-using Core.Interfaces.Sensors;
-using Core.Interfaces.Temperature;
-using Core.Services;
-using Core.Services.Interfaces;
 using Data;
-using Data.Repositories;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<GreenHouseDbContext>();
 
+//Adding core services
 builder.Services.AddCoreServices();
-builder.Services.AddScoped<ITemperatureRepository, TemperatureRepository>();
 
-builder.Services.AddScoped<IThresholdRepository, ThresholdRepository>();
-
-builder.Services.AddScoped<IHumidityRepository, HumidityRepository>();
-
-builder.Services.AddScoped<IDioxideCarbonRepository, DioxideCarbonRepository>();
-
-builder.Services.AddScoped<IMoistureRepository, MoistureRepository>();
-
-builder.Services.AddScoped<IPotRepository, PotRepository>();
-
-builder.Services.AddScoped<ISensorRepository, SensorRepository>();
-
-builder.Services.AddScoped<IGreenhouseRepository, GreenhouseRepository>();
+//Adding repositories
+builder.Services.AddRepositoryServices();
 
 builder.Services.AddScoped<IMessageHandler, MessageHandler>();
 builder.Services.AddHostedService<BridgeMain>();
@@ -48,13 +24,11 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-//}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-//Adding static services 
 
 app.Run();
